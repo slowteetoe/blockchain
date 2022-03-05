c6248d3a93ed71754647235400d5b9b7e92a9f35
Steven Lotito <steven.lotito@gmail.com>
2022-03-05 13:34:22 -0800

    merkle

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ go run main.go createblockchain -address $ADDR1
 # go run main.go reindexutxo
 go run main.go getbalance -address $ADDR1
 go run main.go getbalance -address $ADDR2
-go run main.go send -from $ADDR1 -to $ADDR2 -amount 45
+go run main.go send -from $ADDR1 -to $ADDR2 -amount 5
 go run main.go getbalance -address $ADDR1
 go run main.go getbalance -address $ADDR2
